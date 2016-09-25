0bf663d039fdba286b1d6093a464ac67723c43b4
Tibor Simko <tibor.simko@cern.ch>
2016-09-25 18:10:14 +0200

    docker: nginx front-end with flask app back-end
    Signed-off-by: Tibor Simko <tibor.simko@cern.ch>

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ from base.views import blueprint as main_blueprint
 app = Flask(__name__)
 app._static_folder = 'base/static'
 app.config.update(
-    DEBUG=True,
     JQUERY='',
-    CFG_SITE_NAME='Invenio Software',
+    CFG_SITE_NAME='Invenio',
 )
 app.register_blueprint(main_blueprint)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    app.run()
